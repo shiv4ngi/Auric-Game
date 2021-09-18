52d51c8c9684097adb5054a638ae606c6e2854e7
shiv4ngi <come2shivangu@gmail.com>
2021-09-18 23:20:53 +0530

    Resolved the bug and added test.sh

---
+++ test.sh
@@ -0,0 +1,9 @@
+curl http://localhost:3000/operator
+
+curl http://localhost:3000/operatorGameType?operator=FanDuel
+
+curl http://localhost:3000/operatorName/?operator=DraftKings&operatorGameType=Snake
+
+curl http://localhost:3000/players/operator=DraftKings&operatorName=Main&operatorGameType=Snake
+
+curl http://localhost:3000/players/best?operator=DraftKings&operatorName=Main&operatorGameType=Snake
